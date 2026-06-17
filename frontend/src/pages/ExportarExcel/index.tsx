67226782e3ea4c5ec9455e93c4c437a47fa53d72
Cleiber-ap <cleiber@enovaonline.com.br>
2026-06-17 17:33:13 -0300

    fix: carregar pagamentos em lote no ExportarExcel

--- frontend/src/pages/ExportarExcel/index.tsx
+++ frontend/src/pages/ExportarExcel/index.tsx
@@ -46,15 +46,12 @@ export default function ExportarExcel() {
       setDasSix(d1); setDasEnova(d2)
       setEmpresas(emp)
       setNotas({ six: n1, enova: n2 })
-      const todasNotas = [...n1, ...n2]
-      const pgtoPromises = todasNotas.map((n: any) => {
-        const eid = n1.includes(n) ? 1 : 2
-        return api.get('/notas/pagamentos/' + eid + '/' + n.numero_nf).then((r: any) => ({ nf: n.numero_nf, lista: r.data })).catch(() => ({ nf: n.numero_nf, lista: [] }))
-      })
-      Promise.all(pgtoPromises).then((res: any[]) => {
-        const map: Record<string, any[]> = {}
-        res.forEach(({ nf, lista }) => { map[nf] = lista })
-        setPagamentos(map)
+      // Carregar todos os pagamentos de uma vez
+      Promise.all([
+        api.get('/notas/pagamentos/1').then((r: any) => r.data).catch(() => ({})),
+        api.get('/notas/pagamentos/2').then((r: any) => r.data).catch(() => ({})),
+      ]).then(([pg1, pg2]) => {
+        setPagamentos({ ...pg1, ...pg2 })
       })
     }).finally(() => setLoading(false))
   }, [])
